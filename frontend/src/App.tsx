d766f7089299fba714a25e871d9b3b9b090a9de4
Joel Wembo <joelotepawembo@gmail.com>
2021-07-04 19:53:51 +0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+
+// react frontend application for the book store demo
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
